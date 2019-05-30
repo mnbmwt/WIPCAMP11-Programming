e29ca9118a02d2fbbd228dadbec98e7a6c50417d
mnbmwt <feernlovere@gmail.com>
2019-05-30 11:37:23 +0700

    [ADD] New Project

---
+++ WIP-XI/src/III/Output/Print.java
@@ -0,0 +1,23 @@
+/*
+ * WIP XI Computer Programing 
+ */
+
+package III.Output;
+
+/**
+ *
+ * @author sittiwatlcp
+ */
+public class Print {
+    public static void main(String[] args) {
+        System.out.print("เฟิร์น \n");
+        
+        System.out.print("ครีม \n");
+        
+        // \n <-- new line (ชี้นบรรทัดใหม่)
+        System.out.print("บรีส \n");
+        
+        System.out.print("ซากุระ");
+        
+    }
+}   
